c3bfd02af6e80f8221a47f7d8afd793063bc6a69
ManiGirlTech <imaniwells@Imanis-MacBook-Pro.local>
2024-01-14 20:06:00 -0500

    express app set up

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express')
+
+// express app
+const app = express()
+
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// will respond to a get request
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the property management app'})
+})
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+})
+
+
+// process.env
+
