8f98ba5ce6c49b1e58fe77bd720bd81eff95bbd8
ManiGirlTech <ManiGirlTech@gmail.com>
2024-01-21 14:29:41 -0500

    import Link from react-router-dom

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,12 @@
-
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <header>
             <div className="containder">
+                <Link to='/'>
+                    <h1>Property App</h1>
+                </Link>
 
             </div>
         </header>
