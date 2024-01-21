96b25163b22ab215646607f98ee41edf7a2dafd2
ManiGirlTech <ManiGirlTech@gmail.com>
2024-01-21 14:05:49 -0500

    created a pages folder, created a Home component and imported it into my Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import { BrowserRouter, Routes, Router } from 'react-router-dom';
 
+// pages & components
+import Home from './pages/Home'
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/"
+              element={Home}
+            
+            />
+          </Routes>
+        </div>
 
+
+
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return new (
+       <div className="home">
+        <h2> Home </h2>
+       </div>
+    )
+}
