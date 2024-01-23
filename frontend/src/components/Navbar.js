790671201812ff39a544dfa1b389061265f64ed7
ManiGirlTech <ManiGirlTech@gmail.com>
2024-01-22 23:48:08 -0500

    fetching from property api to home page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <header>
-            <div className="containder">
+            <div className="container">
                 <Link to='/'>
                     <h1>Property App</h1>
                 </Link>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+
+    const [ properties, setProperties] = useState(null)
+    useEffect(() => {
+        const fetchProperties = async () => {
+            // fetch properties from property api
+            const response = await fetch('http://localhost:4000/api/properties')
+            // passes the json response which is the data from properties controller - becomes an array of objects where each object represents a property
+            const json = await response.json()
+            
+
+            // if the repsonse is ok, return
+            if (response.ok) {
+                setProperties(json)
+            }
+
+        }
+
+        fetchProperties()
+    }, []);
+
+
     return (
        <div className="home">
-        <h2> Home </h2>
+        <div className="properties">
+            {properties && properties.map((property) => (
+                <p key={property._id}>{property.address}</p>
+            ))}
+        </div>
        </div>
     )
 }
