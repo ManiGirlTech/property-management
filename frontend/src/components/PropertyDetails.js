889d7087a41bbc71175ff9ad0786523d786884c7
ManiGirlTech <ManiGirlTech@gmail.com>
2024-01-29 23:57:33 -0500

    added image placeholder

--- frontend/src/components/PropertyDetails.js
+++ frontend/src/components/PropertyDetails.js
@@ -1,6 +1,7 @@
 const PropertyDetails = ({ property }) => {
     return (
         <div className="property-details">
+            <img src="https://placehold.jp/150x150.png"  alt="image placeholder"/>
             <h4>{property.address}</h4>
             <p><strong>Price: </strong>${property.price}</p>
             <p><strong>Bedrooms: </strong>{property.price}</p>
