60789fbb99694e1cf17bde6fbdadfb7da5c8b1f4
ManiGirlTech <ManiGirlTech@gmail.com>
2024-02-02 20:32:54 -0500

    created a form to add new property

--- frontend/src/components/PropertyForm.js
+++ frontend/src/components/PropertyForm.js
@@ -6,6 +6,31 @@ const PropertyForm = () => {
     const [bedrooms, setBedrooms] = useState('')
 
     return (
-        
+        <form className="create">
+            <h3>Add a New Property</h3>
+
+            <label>Property Address</label>
+            <input 
+                type="text"
+                onChange={(e) => setAddress(e.target.value)}
+                value={address}
+            />
+
+            <label>Property Price</label>
+            <input 
+                type="number"
+                onChange={(e) => setPrice(e.target.value)}
+                value={price}
+            />
+
+            <label>Bedrooms</label>
+            <input 
+                type="number"
+                onChange={(e) => setBedrooms(e.target.value)}
+                value={bedrooms}
+            />
+
+            <button>Add Property</button>
+        </form>
     )
 }
