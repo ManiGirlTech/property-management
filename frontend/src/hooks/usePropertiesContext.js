0faecd1e5820b75249dc1a02a524e89ea0614ed1
ManiGirlTech <ManiGirlTech@gmail.com>
2024-02-04 18:28:02 -0500

    invoking usePropertiesContext and returning the property context back

--- frontend/src/hooks/usePropertiesContext.js
+++ frontend/src/hooks/usePropertiesContext.js
@@ -1,3 +1,8 @@
 import { PropertiesContext } from "../context/PropertyContext";
 import { useContext } from "react";
 
+export const usePropertiesContext = () => {
+    const context = useContext(PropertiesContext)
+
+    return context
+}
