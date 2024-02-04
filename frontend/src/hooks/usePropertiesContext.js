d5fb7d874955397d652a2d293920617ddb0d5b7e
ManiGirlTech <ManiGirlTech@gmail.com>
2024-02-04 18:29:45 -0500

    added error message

--- frontend/src/hooks/usePropertiesContext.js
+++ frontend/src/hooks/usePropertiesContext.js
@@ -4,5 +4,9 @@ import { useContext } from "react";
 export const usePropertiesContext = () => {
     const context = useContext(PropertiesContext)
 
+    if (!context) {
+        throw Error('usePropertiesContext must be used inside a PropertiesContextProvider')
+    }
+
     return context
 }
